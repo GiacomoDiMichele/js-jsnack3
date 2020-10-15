dc8a4a3799bd345f90f2182d876f9a416df00bb3
Giacomo <giacomo.1936@gmail.com>
2020-10-15 16:39:08 +0200

    modifica ciclo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //creo un ciclo per far ripetere 5 volte la stessa domanda, stampando i risultati della variabile
 for (var i = 0; i < 5; i++) {
-    var lista_numeri = parseInt(prompt("inserisci un numero"))
-    console.log(lista_numeri)
+    var numero_inserito = parseInt(prompt("inserisci un numero"))
+    console.log(numero_inserito)
 }
